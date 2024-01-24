ed6569bbcacd33db5b52c2d25de4624a3291f698
Apichaya Thassamalee <thebem1807@gmail.com>
2024-01-24 22:24:07 +0700

    Add home_page.dart and remove all comments in main.dart

---
+++ lib/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyhomePage extends StetelessWidget {
+  const MyhomePage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
